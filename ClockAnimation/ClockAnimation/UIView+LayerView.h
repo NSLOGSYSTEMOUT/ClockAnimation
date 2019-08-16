2841a20c13778255f2f0f32b54d48313f11dae7d
sai <296600741@qq.com>
2019-08-16 15:36:14 +0800

    添加View分类

---
+++ ClockAnimation/ClockAnimation/UIView+LayerView.h
@@ -0,0 +1,17 @@
+//
+//  UIView+LayerView.h
+//  ClockAnimation
+//
+//  Created by zhangsai on 2019/8/16.
+//  Copyright © 2019 ZS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (LayerView)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ClockAnimation/ClockAnimation/UIView+LayerView.m
@@ -0,0 +1,13 @@
+//
+//  UIView+LayerView.m
+//  ClockAnimation
+//
+//  Created by zhangsai on 2019/8/16.
+//  Copyright © 2019 ZS. All rights reserved.
+//
+
+#import "UIView+LayerView.h"
+
+@implementation UIView (LayerView)
+
+@end
